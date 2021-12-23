af8ddd7110dc8e0a5c64ea8184d9b55cc77e9a8d
Orestis Gartaganis <orestakosg@gmail.com>
2021-12-23 21:40:26 +0100

    Refactored a bit, changed the logic to make it simpler and more realistic

--- contracts/Vaccinator.sol
+++ contracts/Vaccinator.sol
@@ -1,4 +1,6 @@
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.6.0 <0.9.0;
+
+import "github.com/Arachnid/solidity-stringutils/strings.sol";
 
 contract Vaccinator {
     struct VaccinatedPerson {
@@ -8,50 +10,97 @@ contract Vaccinator {
 
     // There are a few addresses that are allowed to verify a QR code
     mapping(address => bool) verifiers;
-    mapping(address => VaccinatedPerson) vaccinatedPeople;
+    // mapping(address => VaccinatedPerson) vaccinatedPeople;
     // The single source of truth is the vaccine serial number. It's unique and has to be contained in 
     // the Health Organization's database. Somehow.
-    mapping(bytes => bool) legitVaccineSerialNumbers;
+    mapping(bytes => string) vaccineSerialNumbersAndNamesMapping;
 
-    // No need to overcharge
-    modifier onlyRegisterOnce(address _personAddress) {
-        require(vaccinatedPeople[_personAddress].name.length == 0);
-        _;
+    constructor(address[] memory _verifiers, bytes[] memory _legitVaccineSerialNumbers){
+        // All the initial addresses should be verifiers
+        for(uint i = 0; i < _verifiers.length; i++) {
+            verifiers[_verifiers[i]] = true;
+        }
+
+        // All the initial vaccination serial numbers are legit and are initialized to "-" name
+        for(uint i = 0; i < _legitVaccineSerialNumbers.length; i++) {
+            vaccineSerialNumbersAndNamesMapping[_legitVaccineSerialNumbers[i]] = "-";
+        }
     }
 
-    // Our super-users
+    // ********** OWNABLE ************ //
+    address private owner = msg.sender; 
+
+    modifier onlyOwner  {
+        require(msg.sender == owner);
+        _;
+    }
+    // /********* OWNABLE ***********/ //
+
+    // No need to overcharge; checking that the user is only registered once
+    // modifier onlyRegisterOnce(address _personAddress) {
+    //     require(vaccinatedPeople[_personAddress].name.length == 0);
+    //     _;
+    // }
+
+    // Our super-users; checking that only super users can verify
     modifier onlyVerifiers(address _verifierAddress) {
         require(verifiers[_verifierAddress]);
         _;
     }
 
-    // Our legit vaccine serial numbers
+    /**
+    * Our legit vaccine serial numbers; checking that only these serial numbers are acceptable
+    * They must: 
+    *  a) be in the list
+    *  b) be so far unclaimed          
+    */
     modifier onlyLegitVaccineSerialNumbers(bytes memory serialNumber) {
-        require(legitVaccineSerialNumbers[serialNumber]);
+        require(keccak256(abi.encode(vaccineSerialNumbersAndNamesMapping[serialNumber])) == keccak256("-"));
         _;
     }
 
-    constructor(address[] memory _verifiers){
-        // All the initial addresses should be verifiers
-        for(uint i = 0; i < _verifiers.length; i++) {
-            verifiers[_verifiers[i]] = true;
-        }
+    // Let's think about it coz should the admin have to pay to register new verifiers?
+    function addVerifier(address newVerifier) public onlyOwner payable {
+        verifiers[newVerifier] = true;
     }
 
+    /// @param _vaccinated A new vaccinated person
+    /// @param name Their name
+    /// @param vaccineSerialNumber A legit vaccine serial number
     // Register Vaccinated Person
-    function registerVaccinatedPerson(address _vaccinated, bytes32 name, bytes memory vaccineSerialNumber) public payable onlyRegisterOnce(_vaccinated) onlyLegitVaccineSerialNumbers(vaccineSerialNumber) returns (string memory qrCode){
+    function registerVaccinatedPerson(address _vaccinated, string memory name, bytes memory vaccineSerialNumber) public payable onlyLegitVaccineSerialNumbers(vaccineSerialNumber) returns (string memory qrCode){
         // QRCode string is produced and returned
-        qrCode = "12343124";
+        qrCode = produceQRCodeText(vaccineSerialNumber);
 
         // The object with the QRCode is stored as a struct
-        vaccinatedPeople[_vaccinated] = VaccinatedPerson(name, vaccineSerialNumber);
+        vaccineSerialNumbersAndNamesMapping[vaccineSerialNumber] = name;
     }
 
+    /// @param qrCode The incoming QR code containing all the necessary info to approve
     // Somehow we need to verify that this person is registered
     function verifyRegisteredPerson(string memory qrCode) public view onlyVerifiers(msg.sender) returns (bytes32 name){
-        // decode qrCode and produce the address!
-        address toVerify = 0x0000000000000000000000000000000000000000;
+        // decode qrCode and produce the serialNumber!
+        val serialNumber = getSlice(0, 9, qrCode);
+
+        // Serial number should be legit
+        name = vaccineSerialNumbersAndNamesMapping[serialNumber];
         
-        name = vaccinatedPeople[toVerify].name;
+        // Should Emit the result
+    }
+
+    function produceQRCodeText(bytes memory serialNumber) pure private returns(string memory qrCodeString) {
+        qrCodeString = string(abi.encodePacked(serialNumber, "pasok"));
+    }
+
+    function decodeQRCodeText(bytes memory qrCodeString) pure private returns(string memory name) {
+        name = string(abi.encodePacked(serialNumber, "pasok"));
+    }
+
+    function getSlice(uint256 begin, uint256 end, string memory text) pure private returns (string memory) {
+        bytes memory a = new bytes(end-begin+1);
+        for(uint i=0;i<=end-begin;i++){
+            a[i] = bytes(text)[i+begin-1];
+        }
+        return string(a);    
     }
 }
